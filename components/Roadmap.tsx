f1479820303d3bf6dc2040c8273305be6d21d295
Divyanshu Yadav <divyanshu.1810@gmail.com>
2022-09-22 22:08:30 +0530

    :bug: Fixed some CSS
    fixed css

--- components/Roadmap.tsx
+++ components/Roadmap.tsx
@@ -7,7 +7,7 @@ import group11 from '../public/Group11.png'
 
 function Roadmap() {
   return (
-    <div className="w-[100vw] flex flex-col justify-center items-center">
+    <div className="w-[100vw] flex flex-col fold_chrono justify-center items-center">
       <div className="md:text-6xl text-4xl md:font-bold font-medium">Road to <span className="text-[#28b3f2]">Alexa</span></div>
       <div className="md:my-12 md:w-[80%] w-[100%]">
         
@@ -15,9 +15,9 @@ function Roadmap() {
           mode="VERTICAL_ALTERNATING"
           cardHeight={120}
           cardWidth={300}
-          lineWidth="5"
-          disableNavOnKey="true"
-          hideControls="true"
+          lineWidth={5}
+          disableNavOnKey={true}
+          hideControls={true}
           theme={{
             primary: 'white',
             secondary: 'white',
