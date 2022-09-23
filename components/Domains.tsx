b3afbe755a953d629190a29aeefbe9bd0771ca17
Divyanshu Yadav <divyanshu.1810@gmail.com>
2022-09-23 10:34:32 +0530

    :lipstick: Fixed CSS
    fixed some css

--- components/Domains.tsx
+++ components/Domains.tsx
@@ -3,7 +3,7 @@ import DomainsImage from './DomainsImage'
 
 function Domains() {
   return (
-    <div className="md:w-[70vw] w-[100vw] ml-auto mb-12 md:mb-32 flex flex-col justify-center items-center">
+    <div id='domains' className="md:w-[70vw] w-[100vw] ml-auto mb-16 md:mb-56 flex flex-col justify-center items-center">
       <div className="md:text-6xl text-4xl md:font-bold font-medium">Our <span className="text-[#28b3f2]">Domains</span></div>
       <div className="flex md:flex-row flex-col md:my-12 my-8 w-full justify-center items-center">
         <div className="md:mx-6 ml-[5rem] md:my-0 my-2 p-2 shrink"><DomainsImage image={'/Frame9.png'}/></div>
