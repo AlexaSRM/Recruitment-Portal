4d36bc55fd4b9e9cd581ce785b44e7e187773fca
Divyanshu Yadav <divyanshu.1810@gmail.com>
2022-09-24 21:08:35 +0530

    :sparkles: Added Register Page
    Register Page

---
+++ pages/register.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function register() {
+  return (
+    <div>
+    <picture className='flex justify-center mt-10 mx-8'>
+      <img src="/alexa.svg" alt="" width='500' />
+    </picture>
+  </div>
+  )
+}
