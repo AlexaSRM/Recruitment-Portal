c3f56cef798e2926d0b9b8e4e7adfb9d04d1b761
Divyanshu Yadav <divyanshu.1810@gmail.com>
2022-09-25 00:59:21 +0530

    :sparkles: Typeform
    Typeform added

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
-
+import * as typeFormEmbed from '@typeform/embed';
+import { useRef,useEffect } from 'react';
 export default function register() {
+  const typeFormRef = useRef(null);
+  useEffect(() => {
+    if(typeFormRef.current){
+          typeFormEmbed.makeWidget(typeFormRef.current, 'https://form.typeform.com/to/cdPFPoHy', {
+    hideFooter: false,
+    hideHeaders: false,
+    opacity: 100,
+  });
+      }
+  }, []);
   return (
     <div>
-    <picture className='flex justify-center mt-10 mx-8'>
-      <img src="/alexa.svg" alt="" width='500' />
-    </picture>
+    
+    <div className='' ref={typeFormRef} style={{ height: '100vh', width: '100%' }}></div>
   </div>
   )
 }
