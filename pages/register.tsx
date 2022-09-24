cd72ce36b0c40f60697f927e7b5f8a08a4dd51a2
Divyanshu Yadav <divyanshu.1810@gmail.com>
2022-09-25 01:29:56 +0530

    :ambulance: Typeform dep
    typeform dep

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,21 +1,12 @@
 import React from 'react'
-import * as typeFormEmbed from '@typeform/embed';
-import { useRef,useEffect } from 'react';
+import { Widget } from '@typeform/embed-react';
 export default function register() {
-  const typeFormRef = useRef(null);
-  useEffect(() => {
-    if(typeFormRef.current){
-          typeFormEmbed.makeWidget(typeFormRef.current, 'https://form.typeform.com/to/cdPFPoHy', {
-    hideFooter: false,
-    hideHeaders: false,
-    opacity: 100,
-  });
-      }
-  }, []);
+const MyComponent = () => {
+  return <Widget id="cdPFPoHy" style={{ height: '100vh', width: '100%' }} className="my-form" />
+}
   return (
     <div>
-    
-    <div className='' ref={typeFormRef} style={{ height: '100vh', width: '100%' }}></div>
-  </div>
+      <MyComponent/>
+    </div>
   )
 }
