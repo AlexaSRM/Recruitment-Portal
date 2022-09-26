1e2a7769643c3ce226cc14ae522b607d7f3f918f
Divyanshu Yadav <divyanshu.1810@gmail.com>
2022-09-26 18:46:53 +0530

    :rocket: Icon Updated
    Updated the icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,13 @@ import Footer from '../components/Footer'
 import Landing from '../components/Landing'
 import Navbar from '../components/Navbar'
 import Roadmap from '../components/Roadmap'
-
+import Head from 'next/head';
 const Home: NextPage = () => {
   return (
     <div className="h-[100%]">
+      <Head>
+        <link rel="shortcut icon" href="/logo.png" />
+      </Head>
       <Navbar/>
       <Landing/>
       <Domains/>
